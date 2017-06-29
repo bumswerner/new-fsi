9377c315cc8ac9ff690451a919d995a3552c852d
Mike Mann <miketheman@t-online.de>
2017-06-29 17:22:23 +0000

    change to io

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626195016) do
+ActiveRecord::Schema.define(version: 20170629103115) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.integer "picture_file_size"
+    t.datetime "picture_updated_at"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
