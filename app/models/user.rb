afa0c39f265667dbaf511ce294be1fbc5ba8cdec
Mike Mann <miketheman@t-online.de>
2017-08-24 21:33:29 +0000

    add two methods to user module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ApplicationRecord
+  
   has_one :profile
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
         
-  #validates_presence_of :name
+  # add virtual attributes
+  validates_presence_of :name
   
   def first_name
-    self.name.split.first_name
+    self.name.split.first
   end
   
   def last_name
-    
+    self.name.split.last 
   end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823204044) do
+ActiveRecord::Schema.define(version: 20170824211621) do
 
   create_table "assoziations", force: :cascade do |t|
     t.string "code"
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 20170823204044) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
     t.boolean "moderator", default: false, null: false
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
