fb119f503847c51d1ab0fa6ab3c80cc8bbb29b51
Mike Mann <miketheman@t-online.de>
2017-08-27 22:28:05 +0000

    add file_field and dropdown feature to material

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,8 @@
 class Material < ApplicationRecord
   belongs_to :section
   belongs_to :lecture
+  
+  has_attached_file :data, styles: { medium: "300x300", thumb: "100x100" }
+  # Validate content type
+  validates_attachment_content_type :data, content_type: /\Aimage\/.*\z/
 end
