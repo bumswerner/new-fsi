8a267b0b82d533195375330c663903ab84d988e6
Mike Mann <miketheman@t-online.de>
2017-08-27 21:23:02 +0000

    add dropdown features to _form

--- app/models/assoziation.rb
+++ app/models/assoziation.rb
@@ -2,4 +2,14 @@ class Assoziation < ApplicationRecord
   belongs_to :category
   belongs_to :courseofstudy
   belongs_to :lecture
+  
+  
+  def course_type_check
+   course = Courseofstudy.all
+   type = Studytype.all
+  # Coursetype
+  # .select('courseofstudy.id, courseofstudy.name, studytypes.name')
+  # .joins(:studytype)
+  # .find_by(courseofstudy.studytype_id => studytype.id) # this should be the last  
+  end
 end
