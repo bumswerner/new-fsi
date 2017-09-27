fd54f60855cde610cebfdc6eabeeba5d1e0822c8
Mike Mann <miketheman@t-online.de>
2017-09-27 15:00:53 +0000

    first level reached

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,14 +1,16 @@
 class Material < ApplicationRecord
   belongs_to :section
   belongs_to :lecture
-  before_validation :data
-  has_attached_file :data, styles: { medium: "300x300", thumb: "100x100" }
+ # before_validation :data
+  has_attached_file :data
  # # Validate content type
  
    before_post_process :check_for_content
    
   validates_attachment :data,
-   content_type: { content_type: ["text/plain", "application/pdf","application/rtf", "image/jpeg", "image/gif", "image/png"] }
+   content_type: { content_type: ["text/plain", "application/pdf","application/rtf", "image/jpeg", "image/gif", "image/png", 
+                                  "video/mpeg", "application/x-rar-compressed", "application/zip", "application/msword",
+                                  	"application/vnd.ms-powerpoint"] }
    
   # do_not_validate_attachment_file_type :data
    
