d7ca3c185be1f1e370ebfe573b9367bf50700d4c
Mike Mann <miketheman@t-online.de>
2017-10-04 23:43:51 +0000

    test4

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => 'stmp.sendgrid.net',
-  :port => '587',
+  :port => 587,
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
